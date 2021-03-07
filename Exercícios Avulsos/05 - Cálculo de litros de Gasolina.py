a5294d3b8f42c4e60d1251828488fbb3dd9fb5be
João Eduardo Pereira <joa0eduardo@hotmail.com>
2021-03-07 09:49:04 -0300

    Ajustado exercício para colocar a função 'destaca_amarelo'

--- Exercícios Avulsos/05 - Cálculo de litros de Gasolina.py
+++ Exercícios Avulsos/05 - Cálculo de litros de Gasolina.py
@@ -1,17 +1,19 @@
 # 5. Um motorista deseja colocar no seu tanque X reais de gasolina. Escreva um algoritmo para ler o preço do
 # litro da gasolina e o valor do pagamento, e exibir quantos litros ele conseguiu colocar no tanque.
 
+from Functions import Colors
+
 valorpago = float(input('Informe o valor que deseja colocar de gasolina: '))
 resposta = ' '
 
-while resposta not in ('SsNn'):
-    resposta = str(input('O valor da Gasolina é R$ 4,94? [S/N]: '))
+while resposta not in 'SN':
+    resposta = str(input('O valor da Gasolina é R$ 4,94? [S/N]: ')).upper()[0]
 
-if resposta in ('Ss'):
+if resposta in 'S':
     litros = valorpago / 4.94
 
 else:
     precogasolina = float(input('Informe o valor atualizado: R$ '))
     litros = valorpago / precogasolina
 
-print(f'Você abastecerá \033[1;33m{litros:.3f}\033[0;0m litros.')
+print(f'Você abastecerá {Colors.destaca_amarelo(litros)} litros.')
