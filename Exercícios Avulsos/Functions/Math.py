09bf98d51b7dc60571d1163bf00e01ce203e9279
João Eduardo Pereira <joa0eduardo@hotmail.com>
2021-03-21 17:29:43 -0300

    Adicionada função de multiplicação

--- Exercícios Avulsos/Functions/Math.py
+++ Exercícios Avulsos/Functions/Math.py
@@ -6,14 +6,24 @@ def soma(list):
         r += n
     return r
 
+
 def area(a, b):
     r = a * b
     return r
+
 
 def calcula_idade(ano):
     r = date.today().year - ano
     return r
 
+
 def calcula_dias_vividos(ano):
     r = calcula_idade(ano) * 365
+    return r
+
+
+def multiplicacao(list):
+    r = 1
+    for n in list:
+        r *= n
     return r
